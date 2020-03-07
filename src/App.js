8f18093ee703e361ea38397ad88c03de9e1bbeea
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-03-07 11:02:19 -0600

    Passed props to Pet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-const Pet = () => {
+const Pet = props => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Mia"),
-    React.createElement("h2", {}, "Cute Dog"),
-    React.createElement("h2", {}, "Maltese")
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.type),
+    React.createElement("h2", {}, props.breed)
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, { name: "Mia", type: "Dog", breed: "Pom" }),
+    React.createElement(Pet, { name: "Doink", type: "Cat", breed: "Mixed" }),
+    React.createElement(Pet, { name: "Drill", type: "Dog", breed: "Lab" })
   ]);
 };
 
