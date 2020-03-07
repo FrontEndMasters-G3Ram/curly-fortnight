cf25ac517a84714b991021284dbd610071ec7d05
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-03-07 10:57:24 -0600

    Added a Pet component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Mia"),
+    React.createElement("h2", {}, "Cute Dog"),
+    React.createElement("h2", {}, "Maltese")
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet)
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
