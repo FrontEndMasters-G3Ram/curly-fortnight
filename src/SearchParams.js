3fecdfc4ad9b5f02bc2ee5df76a41d64835be671
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-03-08 18:18:27 -0500

    Added select statement

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 function SearchParam() {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -16,8 +18,24 @@ function SearchParam() {
             placeholder="Location"
             onChange={e => setLocation(e.target.value)}
           />
-          <button>Submit</button>
         </label>
+        <label htmlFor="animal">
+          <select
+            name="animal"
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option value="All">All</option>
+            {ANIMALS.map((animal, index) => (
+              <option value={animal} key={index}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
       </form>
     </div>
   );
