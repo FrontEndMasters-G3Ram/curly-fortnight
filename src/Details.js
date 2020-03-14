8bd7966878acdfdf2926dfd7adcd5ea66156cfd9
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-03-14 08:57:35 -0500

    Adding details page

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,34 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-function Details(props) {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const myAnimal = await pet.animal(this.props.id);
+      this.setState({
+        name: myAnimal.name,
+        animal: myAnimal.type,
+        location: `${myAnimal.contact.address.city}, ${myAnimal.contact.address.state}`,
+        description: myAnimal.description,
+        media: myAnimal.photos,
+        breed: myAnimal.breeds.primary,
+        loading: false
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return <div>Details</div>;
+  }
 }
 
 export default Details;
